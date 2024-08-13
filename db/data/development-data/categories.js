e218cd42569d10f2bf3215d099d3994af72096ea
Billy Dentith <billydentith@hotmail.co.uk>
2024-08-13 23:25:32 +0100

    Modifies test and development data

--- db/data/development-data/categories.js
+++ db/data/development-data/categories.js
@@ -1,6 +1,6 @@
 module.exports = [
-    { id: '1', title: 'Italian Pastas', desc: 'Savor the taste of perfection with our exquisite Italian handmade pasta menu.', image: "/temporary/m1.png", slug: 'appetizers', color: '#FF5733' },
-    { id: '2', title: 'Juicy Burgers', desc: 'Burger Bliss: Juicy patties, bold flavors, and gourmet toppings galore.', image: "/temporary/m2.png", slug: 'main-courses', color: '#33FF57' },
-    { id: '3', title: 'Cheesy Pizzas', desc: 'Pizza Paradise: Irresistible slices, mouthwatering toppings, and cheesy perfection.', image: "/temporary/m3.png", slug: 'desserts', color: '#3357FF' },
+    { id: '1', title: 'Italian Pastas', desc: 'Savor the taste of perfection with our exquisite Italian handmade pasta menu.', image: "/temporary/m1.png", slug: 'pastas', color: '#FF5733' },
+    { id: '2', title: 'Juicy Burgers', desc: 'Burger Bliss: Juicy patties, bold flavors, and gourmet toppings galore.', image: "/temporary/m2.png", slug: 'burgers', color: '#33FF57' },
+    { id: '3', title: 'Cheesy Pizzas', desc: 'Pizza Paradise: Irresistible slices, mouthwatering toppings, and cheesy perfection.', image: "/temporary/m3.png", slug: 'pizzas', color: '#3357FF' },
   ];
   
--- db/data/development-data/products.js
+++ db/data/development-data/products.js
@@ -1,141 +1,259 @@
 module.exports = [
-    {
-      id: '1',
-      title: 'Sicilian',
-      desc: 'Ignite your taste buds with a fiery combination of spicy pepperoni, jalapeños, crushed red pepper flakes, and melted mozzarella cheese, delivering a kick with every bite.',
-      image: '/temporary/p1.png',
-      price: 2490,
-      isFeatured: false,
-      options: [
-        {
-          title: 'Small',
-          additionalPrice: 0,
-        },
-        {
-          title: 'Medium',
-          additionalPrice: 400,
-        },
-        {
-          title: 'Large',
-          additionalPrice: 600,
-        },
-      ],
-      catSlug: 'pizzas',
-    },
-    {
-      id: '2',
-      title: 'Mediterranean Delight',
-      desc: 'Embark on a culinary journey with this Mediterranean-inspired creation, featuring zesty feta cheese, Kalamata olives, sun-dried tomatoes, and a sprinkle of oregano.',
-      image: '/temporary/p8.png',
-      price: 3290,
-      isFeatured: false,
-      options: [
-        {
-          title: 'Small',
-          additionalPrice: 0,
-        },
-        {
-          title: 'Medium',
-          additionalPrice: 400,
-        },
-        {
-          title: 'Large',
-          additionalPrice: 600,
-        },
-      ],
-      catSlug: 'pizzas',
-    },
-    {
-      id: '3',
-      title: 'Bella Napoli',
-      desc: 'A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.',
-      image: '/temporary/p3.png',
-      price: 2690,
-      isFeatured: false,
-      options: [
-        {
-          title: 'Small',
-          additionalPrice: 0,
-        },
-        {
-          title: 'Medium',
-          additionalPrice: 400,
-        },
-        {
-          title: 'Large',
-          additionalPrice: 600,
-        },
-      ],
-      catSlug: 'pizzas',
-    },
-    {
-      id: '4',
-      title: 'Pesto Primavera',
-      desc: 'A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.',
-      image: '/temporary/p10.png',
-      price: 2890,
-      isFeatured: false,
-      options: [
-        {
-          title: 'Small',
-          additionalPrice: 0,
-        },
-        {
-          title: 'Medium',
-          additionalPrice: 400,
-        },
-        {
-          title: 'Large',
-          additionalPrice: 600,
-        },
-      ],
-      catSlug: 'pizzas',
-    },
-    {
-      id: '5',
-      title: 'Veggie Supreme',
-      desc: 'A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.',
-      image: '/temporary/p11.png',
-      price: 2490,
-      isFeatured: false,
-      options: [
-        {
-          title: 'Small',
-          additionalPrice: 0,
-        },
-        {
-          title: 'Medium',
-          additionalPrice: 400,
-        },
-        {
-          title: 'Large',
-          additionalPrice: 600,
-        },
-      ],
-      catSlug: 'pizzas',
-    },
-    {
-      id: '6',
-      title: 'Four Cheese Fantasy',
-      desc: 'Experience pure cheesy bliss with a melty blend of mozzarella, cheddar, provolone, and Parmesan cheeses, creating a rich and indulgent pizza experience.',
-      image: '/temporary/p12.png',
-      price: 2290,
-      isFeatured: false,
-      options: [
-        {
-          title: 'Small',
-          additionalPrice: 0,
-        },
-        {
-          title: 'Medium',
-          additionalPrice: 400,
-        },
-        {
-          title: 'Large',
-          additionalPrice: 600,
-        },
-      ],
-      catSlug: 'pizzas',
-    },
-  ];
-  
+  // Existing pizza items...
+  {
+    id: '1',
+    title: 'Sicilian',
+    desc: 'Ignite your taste buds with a fiery combination of spicy pepperoni, jalapeños, crushed red pepper flakes, and melted mozzarella cheese, delivering a kick with every bite.',
+    image: '/temporary/p1.png',
+    price: 2490,
+    isFeatured: false,
+    options: [
+      {
+        title: 'Small',
+        additionalPrice: 0,
+      },
+      {
+        title: 'Medium',
+        additionalPrice: 400,
+      },
+      {
+        title: 'Large',
+        additionalPrice: 600,
+      },
+    ],
+    catSlug: 'pizzas',
+  },
+  {
+    id: '2',
+    title: 'Mediterranean Delight',
+    desc: 'Embark on a culinary journey with this Mediterranean-inspired creation, featuring zesty feta cheese, Kalamata olives, sun-dried tomatoes, and a sprinkle of oregano.',
+    image: '/temporary/p8.png',
+    price: 3290,
+    isFeatured: false,
+    options: [
+      {
+        title: 'Small',
+        additionalPrice: 0,
+      },
+      {
+        title: 'Medium',
+        additionalPrice: 400,
+      },
+      {
+        title: 'Large',
+        additionalPrice: 600,
+      },
+    ],
+    catSlug: 'pizzas',
+  },
+  {
+    id: '3',
+    title: 'Bella Napoli',
+    desc: 'A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.',
+    image: '/temporary/p3.png',
+    price: 2690,
+    isFeatured: false,
+    options: [
+      {
+        title: 'Small',
+        additionalPrice: 0,
+      },
+      {
+        title: 'Medium',
+        additionalPrice: 400,
+      },
+      {
+        title: 'Large',
+        additionalPrice: 600,
+      },
+    ],
+    catSlug: 'pizzas',
+  },
+  {
+    id: '4',
+    title: 'Pesto Primavera',
+    desc: 'A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.',
+    image: '/temporary/p10.png',
+    price: 2890,
+    isFeatured: false,
+    options: [
+      {
+        title: 'Small',
+        additionalPrice: 0,
+      },
+      {
+        title: 'Medium',
+        additionalPrice: 400,
+      },
+      {
+        title: 'Large',
+        additionalPrice: 600,
+      },
+    ],
+    catSlug: 'pizzas',
+  },
+  {
+    id: '5',
+    title: 'Veggie Supreme',
+    desc: 'A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.',
+    image: '/temporary/p11.png',
+    price: 2490,
+    isFeatured: false,
+    options: [
+      {
+        title: 'Small',
+        additionalPrice: 0,
+      },
+      {
+        title: 'Medium',
+        additionalPrice: 400,
+      },
+      {
+        title: 'Large',
+        additionalPrice: 600,
+      },
+    ],
+    catSlug: 'pizzas',
+  },
+  {
+    id: '6',
+    title: 'Four Cheese Fantasy',
+    desc: 'Experience pure cheesy bliss with a melty blend of mozzarella, cheddar, provolone, and Parmesan cheeses, creating a rich and indulgent pizza experience.',
+    image: '/temporary/p12.png',
+    price: 2290,
+    isFeatured: false,
+    options: [
+      {
+        title: 'Small',
+        additionalPrice: 0,
+      },
+      {
+        title: 'Medium',
+        additionalPrice: 400,
+      },
+      {
+        title: 'Large',
+        additionalPrice: 600,
+      },
+    ],
+    catSlug: 'pizzas',
+  },
+
+  // New burger items...
+  {
+    id: '7',
+    title: 'Classic Cheeseburger',
+    desc: 'Savour the timeless taste of our Classic Cheeseburger, featuring a juicy beef patty topped with melted cheddar, fresh lettuce, tomatoes, onions, and a dollop of tangy mayo.',
+    image: '/temporary/p9.png',
+    price: 1990,
+    isFeatured: true,
+    options: [
+      {
+        title: 'Single',
+        additionalPrice: 0,
+      },
+      {
+        title: 'Double',
+        additionalPrice: 500,
+      },
+    ],
+    catSlug: 'burgers',
+  },
+  {
+    id: '8',
+    title: 'BBQ Bacon Burger',
+    desc: 'Indulge in smoky perfection with our BBQ Bacon Burger, featuring crispy bacon, tangy BBQ sauce, melted cheese, and a juicy beef patty.',
+    image: '/temporary/p2.png',
+    price: 2490,
+    isFeatured: false,
+    options: [
+      {
+        title: 'Single',
+        additionalPrice: 0,
+      },
+      {
+        title: 'Double',
+        additionalPrice: 500,
+      },
+    ],
+    catSlug: 'burgers',
+  },
+  {
+    id: '9',
+    title: 'Spicy Chicken Burger',
+    desc: 'Turn up the heat with our Spicy Chicken Burger, a crispy chicken fillet topped with spicy mayo, fresh lettuce, and pickles, all packed in a toasted bun.',
+    image: '/temporary/p5.png',
+    price: 2290,
+    isFeatured: false,
+    options: [
+      {
+        title: 'Single',
+        additionalPrice: 0,
+      },
+      {
+        title: 'Double',
+        additionalPrice: 500,
+      },
+    ],
+    catSlug: 'burgers',
+  },
+
+  // New pasta items...
+  {
+    id: '10',
+    title: 'Creamy Alfredo Pasta',
+    desc: 'Delight in rich, creamy Alfredo sauce over tender fettuccine pasta, topped with grilled chicken and freshly grated Parmesan cheese.',
+    image: '/temporary/p7.png',
+    price: 2790,
+    isFeatured: false,
+    options: [
+      {
+        title: 'Regular',
+        additionalPrice: 0,
+      },
+      {
+        title: 'Large',
+        additionalPrice: 500,
+      },
+    ],
+    catSlug: 'pastas',
+  },
+  {
+    id: '11',
+    title: 'Spaghetti Bolognese',
+    desc: 'Enjoy a classic Italian dish with our Spaghetti Bolognese, featuring savory ground beef in a rich tomato sauce served over al dente spaghetti.',
+    image: '/temporary/p7.png',
+    price: 2590,
+    isFeatured: false,
+    options: [
+      {
+        title: 'Regular',
+        additionalPrice: 0,
+      },
+      {
+        title: 'Large',
+        additionalPrice: 500,
+      },
+    ],
+    catSlug: 'pastas',
+  },
+  {
+    id: '12',
+    title: 'Pesto Penne',
+    desc: 'Taste the freshness of basil in our Pesto Penne, with a deliciously creamy pesto sauce coating perfectly cooked penne pasta, garnished with cherry tomatoes and pine nuts.',
+    image: '/temporary/p7.png',
+    price: 2690,
+    isFeatured: false,
+    options: [
+      {
+        title: 'Regular',
+        additionalPrice: 0,
+      },
+      {
+        title: 'Large',
+        additionalPrice: 500,
+      },
+    ],
+    catSlug: 'pastas',
+  },
+];
