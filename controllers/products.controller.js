ece9a2df30e76016c57319c233edc1d5d7d6c1ec
Billy Dentith <billydentith@hotmail.co.uk>
2025-01-29 21:07:52 +0000

    Adds edit product functionality

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,4 +1,4 @@
-const { selectProducts, selectSingleProduct, removeProductById, addProduct } = require('../models/products.models');
+const { selectProducts, selectSingleProduct, removeProductById, addProduct, editProduct } = require('../models/products.models');
 
 exports.getAllProducts = (req, res, next) => {
     const { category } = req.query;
@@ -42,4 +42,30 @@ exports.postProduct = (req, res, next) => {
     } else {
         res.status(400).send({ message: "Invalid Product"})
     }
+}
+
+exports.patchProductById = (req, res, next) => {
+    const { id } = req.params;
+    const updates = req.body;
+    
+    const acceptedProperties = ['title', 'description', 'image', 'price', 'is_featured', 'options']
+
+    if (!updates || Object.keys(updates).length === 0) {
+        return res.status(400).send({ message: "Request body cannot be empty" });
+    }
+
+    const isValid = Object.keys(updates).every((key) => acceptedProperties.includes(key));
+
+    if (!isValid) {
+        return res.status(400).send({ message: "Invalid product" });
+    }
+   
+    editProduct(id, updates)
+        .then((product) => {        
+            return res.status(200).send({ product })
+        })
+        .catch((err) => {
+            console.error("Error in editProduct:", err); 
+            next(err); 
+        });
 }
