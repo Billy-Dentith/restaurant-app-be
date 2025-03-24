6c89e36841d38f12f320c1a52c7108964890d66c
Billy Dentith <billydentith@hotmail.co.uk>
2025-03-24 16:37:08 +0000

    Adds cors

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const apiRouter = require('./routes/api-router')
 const { handleInvalidEndpoint, handleCustomErrors, handleServerErrors, handlePsqlErrors } = require('./errors');
@@ -7,6 +8,7 @@ const ordersRouter = require('./routes/orders-router');
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/api', apiRouter);
